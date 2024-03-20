67e252fe96e88ee3c8c4e0e7eb118b89597e347f
Steam Deck User <colombraf@gmail.com>
2024-03-20 11:18:10 +0100

    modified joystickCtrl. the robot waves via button press

--- scripts/joystickCtrl.sh
+++ scripts/joystickCtrl.sh
@@ -1,4 +1,12 @@
 date > ~/r1-steamdeck-launcher/logs/joystickCtrl.txt
+
+if docker exec tourCore2 pgrep -x joystickCtrl > /dev/null
+then
+	docker exec tourCore2 sudo pkill -f joystickCtrl
+	echo "Found running joystickCtrl, killing it" >> ~/r1-steamdeck-launcher/logs/joystickCtrl.txt
+	sleep 2
+fi
+
 docker exec tourCore2 joystickCtrl --from /home/user1/mnt/joystick_steamdeck.ini --GENERAL::outputPortName /joystickSteamdeck:o &
 sleep 1;
 docker exec tourCore2 yarp connect /joystickSteamdeck:o /baseControl/input/joystick/data:i tcp+recv.portmonitor+type.dll+file.joy2vel >> ~/r1-steamdeck-launcher/logs/joystickCtrl.txt &
--- scripts/ssh-robot-yarprun.sh
+++ scripts/ssh-robot-yarprun.sh
@@ -2,7 +2,7 @@ ssh r1-user@r1-base << EOF
 tmux new -d -s yarpserver yarpserver
 tmux new -d -s yarprun
 tmux send-keys -t  yarprun "docker run --rm -it --privileged --network host --pid host -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -e QT_X11_NO_MITSHM=1 --name r1base_tourCore2 elandini84/r1images:tourCore2_ubuntu22.04_iron_devel" Enter
-sleep 1;
+sleep 3;
 tmux send-keys -t  yarprun "yarprun --server /r1-base --log" Enter
 EOF
 
